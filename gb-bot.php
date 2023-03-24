a9681592a95c009daa474c987dba71d97d04fd9b
luiscard91 <luis@generationsbeyond.com>
2023-03-24 13:48:28 -0400

    Version control

--- gb-bot.php
+++ gb-bot.php
@@ -3,7 +3,7 @@
 * Plugin Name: GB&bull;BOT
 * Plugin URI: https://generationsbeyond.com/gb-bot/
 * Description: Make your website do more stuff.
-* Version: 1.2.1
+* Version: 1.3.0
 * Author: Generations Beyond
 * Author URI: https://generationsbeyond.com/
 * License: GPLv3
