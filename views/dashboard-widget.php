8033bcc2f2cdc65e004f840b3a8f4c7b875dccfa
Harry Scheiner <harryscheiner@gmail.com>
2023-12-19 13:32:33 -0500

    Hide "Edit Settings" button on dashboard widget for non-admins

--- views/dashboard-widget.php
+++ views/dashboard-widget.php
@@ -1,5 +1,9 @@
-<img style="width: 200px; max-width: 100%; height: auto; display: block; margin: 0 auto;" src="<?php echo $this->plugin->url; ?>img/gb-bot.png" alt="GB&bull;BOT"/>
+<img style="width: 200px; max-width: 100%; height: auto; display: block; margin: 0 auto;" src="<?= $this->plugin->url; ?>img/gb-bot.png" alt="GB&bull;BOT"/>
 
 <div style="margin-top: 10px; text-align: center;">
-	<a href="admin.php?page=gb-bot" class="button button-primary"><span style="vertical-align: middle; font-size: 17px;" aria-hidden="true" class="dashicons dashicons-edit"></span> Edit Settings</a>
+	<?php if (current_user_can('level_10')) : ?>
+		<a href="admin.php?page=gb-bot" class="button button-primary">
+			<span style="vertical-align: middle; font-size: 17px;" aria-hidden="true" class="dashicons dashicons-edit"></span> Edit Settings
+		</a>
+	<?php endif; ?>
 </div>
